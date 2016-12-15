206fd23f8e090abe0d74b35d9c3ef3253c0ee3d3
lgybetter <437675103@qq.com>
2016-12-15 23:40:17 +0800

    Install nvm with the file

--- initNvm.sh
+++ initNvm.sh
@@ -1,6 +1,3 @@
 #fetch nvm to control node version
 echo "fetch nvm intsall bash"
-git clone git@github.com:creationix/nvm.git ~/.nvm
-cd ~/.nvm
-git checkout `git describe --abbrev=0 --tags`
-./install.sh
+.installNvm.sh
